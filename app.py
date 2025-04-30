241860c1233ba87f762c4231765aebdbd36c94bc
Kstephens24CDW <94698899+Kstephens24CDW@users.noreply.github.com>
2025-04-30 16:53:42 +0100

    updates

--- app.py
+++ app.py
@@ -2,33 +2,20 @@ import os
 # Azure OpenAI
 from langchain_openai import AzureChatOpenAI
 # OpenAI
-#from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
 
-from azure.identity import DefaultAzureCredential, get_bearer_token_provider
-
-token_provider = get_bearer_token_provider(
-    DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
-)
-
-llm = AzureChatOpenAI(
-    api_version=api_version,
-    azure_deployment=azure_deployment,
-    azure_endpoint=azure_endpoint,
-    model_name=model_name,
-    azure_ad_token_provider=token_provider
-)
 # Azure OpenAI
 azure_endpoint = os.environ['AZURE_OPENAI_ENDPOINT']
 azure_deployment = os.environ['AZURE_OPENAI_DEPLOYMENT_NAME']
 model_name = os.environ['OPENAI_MODEL_NAME']
 api_version = os.environ['AZURE_OPENAI_API_VERSION']
 # Only needed if you're not using a managed identity
-#api_key = os.environ['openaikey']
+#api_key = os.environ['OPENAI_API_KEY']
 
 # OpenAI
 # api_key = os.environ['OPENAI_API_KEY']
@@ -53,7 +40,7 @@ def hello():
         azure_deployment=azure_deployment,
         model_name=model_name,
         api_version=api_version,
-        #api_key=openaikey
+        #api_key=api_key
     )
     text = llm.invoke(req).content
 
