c965d269f1aabd860cf5473689e8f524979e63b6
Kstephens24CDW <94698899+Kstephens24CDW@users.noreply.github.com>
2025-04-30 15:45:46 +0100

    openai

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
 import os
+# Azure OpenAI
+from langchain_openai import AzureChatOpenAI
+# OpenAI
+from langchain_openai import ChatOpenAI
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
 
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+token_provider = get_bearer_token_provider(
+    DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+)
+
+llm = AzureChatOpenAI(
+    api_version=api_version,
+    azure_deployment=azure_deployment,
+    azure_endpoint=azure_endpoint,
+    model_name=model_name,
+    azure_ad_token_provider=token_provider
+)
+# Azure OpenAI
+azure_endpoint = os.environ['AZURE_OPENAI_ENDPOINT']
+azure_deployment = os.environ['AZURE_OPENAI_DEPLOYMENT_NAME']
+model_name = os.environ['OPENAI_MODEL_NAME']
+api_version = os.environ['AZURE_OPENAI_API_VERSION']
+# Only needed if you're not using a managed identity
+#api_key = os.environ['openaikey']
+
+# OpenAI
+# api_key = os.environ['OPENAI_API_KEY']
 
 @app.route('/')
 def index():
@@ -18,15 +45,28 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+    req = request.form.get('req')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    # Azure OpenAI
+    llm = AzureChatOpenAI(
+        azure_endpoint=azure_endpoint,
+        azure_deployment=azure_deployment,
+        model_name=model_name,
+        api_version=api_version,
+        #api_key=openaikey
+    )
+    text = llm.invoke(req).content
 
+    # OpenAI
+    #llm = ChatOpenAI(openai_api_key=api_key)
+    #text = llm.invoke(req).content
+
+    if req:
+        print('Request for hello page received with req=%s' % req)
+        return render_template('hello.html', req=text)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
